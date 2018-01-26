da582da02d4c1772ed0f40dabc182c46b9dca0ea
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-01-26 15:17:12 +0530

    Create printpartten.c

---
+++ printpartten.c
@@ -0,0 +1,28 @@
+ /* print the *partten
+ * * * * *
+ * *   * *
+ *   *   *
+ * *   * *
+ * * * * *   */
+ 
+ #include <stdio.h>
+
+int main(void) {
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if((i==1&&j==2)||(i==2&&j==1)||(i==2&&j==3)||(i==3&&j==2))
+			{
+				printf("\t");
+			}
+			else
+			{
+			printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
